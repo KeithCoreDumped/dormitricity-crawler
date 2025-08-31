174b7377b2cd021f5bb30c2971712309bea0d73f
KeithCoreDumped <2385402800@qq.com>
2025-08-31 23:59:19 +0800

    fix keyError & api inconsistency between campus

--- legacy_backend.py
+++ legacy_backend.py
@@ -13,14 +13,14 @@ class legacy_backend:
         # fetch_failure = RuntimeError("Fetch Failure")
         if dorm.campus == "沙河":
             p = "南" if dorm.bld.startswith("S") else "北"
-            partment_id = f"沙河校区雁{p}园{dorm.bld}楼"
+            part_id = f"沙河校区雁{p}园{dorm.bld}楼"
             floor_id = f"{dorm.floor}层"
             area_id = "2"
             # fetch dorm_id
             res = requests.post(
                 "https://app.bupt.edu.cn/buptdf/wap/default/drom",
                 data={
-                    "partmentId": partment_id,
+                    "partmentId": part_id,
                     "floorId": floor_id,
                     "areaid": area_id
                 },
@@ -46,7 +46,7 @@ class legacy_backend:
             if r["e"] != 0:
                 raise FetchFailure(r["m"])
             dorm_ids: list[dict[str, str]] = r["d"]["data"]
-            dorm_id = next(o["dormNum"] for o in dorm_ids if dorm.room in o["dormName"])
+            dorm_id = next(o["dromNum"] for o in dorm_ids if dorm.room in o["dromName"])
         else:
             part_name = f"学{to_cn_number(int(dorm.bld))}楼"
             floor_id = f"{dorm.floor}层"
@@ -109,13 +109,19 @@ class legacy_backend:
         if r["e"] != 0:
             raise FetchFailure(r["m"])
         data = r["d"]["data"]
-        kwh = data["surplus"] + data["freeEnd"]  # 剩余电量 + 剩余赠送电量
+        kwh = float(data["surplus"])
+        if dorm.campus == "西土城":
+            kwh += float(data["freeEnd"])
+        # freeEnd 在沙河是「总用电量」
+        #         在西土城是「剩余赠送电量」
+        # freeEnd 和 surplus 在沙河是字符串
+        #                    在西土城是浮点数
         ts = datetime.fromisoformat(data["time"])
         return kwh, ts
     
 if __name__ == "__main__":
     from secret import legacy_backend_cookies as cookies
     b = legacy_backend(cookies)
-    d = dorm_info("6-120")
+    d = dorm_info("E-505")
     q = b.query(d)
     print(q)
